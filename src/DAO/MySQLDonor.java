1bc374e19b60adb93068a99cd751353589ceaaf6
AlohaDodo <d00265290@student.dkit.ie>
2025-07-28 20:40:37 +0100

    DAO donor result set extract

--- src/DAO/MySQLDonor.java
+++ src/DAO/MySQLDonor.java
@@ -1,4 +1,19 @@
 package DAO;
 
+import DTO.Donor;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class MySQLDonor {
+
+    //Helper method to extract Donor from Result Set
+    private Donor extractDonorFromResultSet(ResultSet rs) throws SQLException {
+        return new Donor(
+                rs.getInt("id"),
+                rs.getString("firstName"),
+                rs.getString("secondName"),
+                rs.getString("teleNumber")
+        );
+    }
 }
