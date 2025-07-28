0acd66236caab48ba93bf43975585ce7e5b69a0b
AlohaDodo <d00265290@student.dkit.ie>
2025-07-28 19:58:14 +0100

    Wrote donor DTO

--- src/DTO/Donor.java
+++ src/DTO/Donor.java
@@ -1,4 +1,61 @@
 package DTO;
 
 public class Donor {
+    private int donorId;
+    private String firstName;
+    private String lastName;
+    private String telephone;
+
+    //Constructor
+    public Donor(int donorId, String firstName, String lastName, String telephone) {
+        this.donorId = donorId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.telephone = telephone;
+    }
+
+    //Getters
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public int getDonorId() {
+        return donorId;
+    }
+
+    //Setters
+    public void setDonorId(int donorId) {
+        this.donorId = donorId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "Donor{" +
+                "donorId=" + donorId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", telephone='" + telephone + '\'' +
+                '}';
+    }
 }
